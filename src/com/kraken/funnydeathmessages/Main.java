04386b10cc6e20bdb81eb6e2b2a3f47535124d37
Randall Arms <contact.randallArms@gmail.com>
2017-01-24 07:23:52 -0800

    v1.1 | Added default "/fdm" command
    - Added default "/fdm" command for name, byline, and version #

--- src/com/kraken/funnydeathmessages/Main.java
+++ src/com/kraken/funnydeathmessages/Main.java
@@ -1,5 +1,5 @@
 // ==========================================================================
-// |FUNNY DEATH MESSAGES v1.0.1
+// |FUNNY DEATH MESSAGES v1.1
 // | by Kraken | https://www.spigotmc.org/resources/funnydeathmessages.35291/
 // | code inspired by various Bukkit & Spigot devs -- thank you. 
 // |
@@ -10,6 +10,10 @@
 
 package com.kraken.funnydeathmessages;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -29,6 +33,36 @@ public class Main extends JavaPlugin {
     public void onDisable() {
     	
         getLogger().info("FunnyDeathMessages has been disabled.");
+        
+    }
+    
+  //FDM commands
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+    	String command = cmd.getName();
+		Player player = (Player) sender;
+        
+      //Player commands
+        if ( sender instanceof Player ) {
+        	
+        	switch (command) {
+        	
+			  //Command: fdm
+        	    case "fdm":
+        			  
+        	        player.sendMessage(ChatColor.RED + "[FDM]" + ChatColor.GRAY + " | FunnyDeathMessages | Laughter is the best medicine, so yuck it up (v1.1)");
+			        return true;
+			        
+			    default:
+			    	  
+			        player.sendMessage(ChatColor.RED + "[FDM]" + ChatColor.GRAY + " | " + "Command not recognized.");
+			        return true;
+			    
+	        }
+        
+        }
+        
+        return true;
         
     }
     
